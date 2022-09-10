ae86bc0b82a1194cad8b3ade36e65d9eaafbbfd2
vladislav <ve@incsub.com>
2022-09-10 08:36:30 +0200

    Expose sine generator

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -4,10 +4,23 @@ pub trait Generator {
     fn sample_at(&self, t: f64, frequency: f64, volume: f64) -> f64;
 }
 
-struct Sine;
+pub struct Sine;
 
 impl Generator for Sine {
     fn sample_at(&self, t: f64, frequency: f64, volume: f64) -> f64 {
         volume * (frequency * t * 2.0 * PI).sin()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sine_values() {
+        let osc = Sine {};
+        for t in 0..10 {
+            eprintln!("{}: {}", t, osc.sample_at(t as f64, 440.0, 1.0));
+        }
     }
 }
