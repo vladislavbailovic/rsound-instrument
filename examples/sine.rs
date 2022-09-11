1d3ca7fb1336281b513320e611cbecd4e1cfcec0
vladislav <ve@incsub.com>
2022-09-11 07:16:45 +0200

    Use line graph

--- examples/sine.rs
+++ examples/sine.rs
@@ -5,18 +5,18 @@ use graph::svg::Renderer;
 #[cfg(feature = "graph")]
 use graph::writer::{FileWriter, Writer};
 #[cfg(feature = "graph")]
-use graph::{Block, Graph, Hits};
+use graph::{Block, Graph, Line};
 
 
 #[cfg(feature = "graph")]
 fn main() -> std::io::Result<()> {
     let synth = Instrument::new(generator::Sine {}, envelope::Fixed {});
-    let values: Vec<Block> = synth.play(440.0, 1.0, 1.0)
+    let values: Vec<Block> = synth.play(440.0, 0.01, 1.0)
     .iter()
-    .map(|x| Block::new(1.0, (1.0+x)*10.0))
+    .map(|y| Block::new(1.0, y + 1.0))
     .collect();
 
-    let hits = Hits::new(&values);
+    let hits = Line::new(&values);
     let w = FileWriter::new("foo.svg");
     let renderer = Renderer::new(&hits.size());
     w.write(renderer, hits)?;
