c5fe1ea7b183b5d62dd899ceed5e2792597614b3
vladislav <ve@incsub.com>
2022-09-17 08:59:52 +0200

    Convert stream write results to options

--- examples/sound.rs
+++ examples/sound.rs
@@ -37,65 +37,65 @@ impl OutputRenderer for WaveRenderer {
         let block_align = bits_per_sample / 8;
         let mut buf = Vec::new();
 
-        buf.write(b"RIFF");
+        buf.write(b"RIFF").ok()?;
 
         assert_eq!(buf.len(), 4, "4|4|ChunkSize|44 + SubChunk2Size");
-        buf.write(&(fsize as u32).to_le_bytes());
+        buf.write(&(fsize as u32).to_le_bytes()).ok()?;
 
         assert_eq!(buf.len(), 8, "8|4|Format|Contains the letters 'WAVE'");
-        buf.write(b"WAVE");
-        buf.write(b"fmt ");
+        buf.write(b"WAVE").ok()?;
+        buf.write(b"fmt ").ok()?;
 
         assert_eq!(buf.len(), 16, "16|4|Subchunk1Size|16 for PCM");
-        buf.write(&16_u32.to_le_bytes());
+        buf.write(&16_u32.to_le_bytes()).ok()?;
 
         assert_eq!(
             buf.len(),
             20,
             "20|2|AudioFormat|PCM = 1 (i.e. Linear quantization)"
         );
-        buf.write(&1_u16.to_le_bytes());
+        buf.write(&1_u16.to_le_bytes()).ok()?;
 
         assert_eq!(buf.len(), 22, "22|2|NumChannels|Mono = 1, Stereo = 2, etc.");
-        buf.write(&1_u16.to_le_bytes());
+        buf.write(&1_u16.to_le_bytes()).ok()?;
 
         assert_eq!(buf.len(), 24, "24|4|SampleRate|8000, 44100, etc.");
-        buf.write(&(SAMPLE_RATE as u32).to_le_bytes());
+        buf.write(&(SAMPLE_RATE as u32).to_le_bytes()).ok()?;
 
         assert_eq!(
             buf.len(),
             28,
             "28|4|ByteRate|== SampleRate * NumChannels * BitsPerSample/8"
         );
-        buf.write(&(byte_rate as u32).to_le_bytes());
+        buf.write(&(byte_rate as u32).to_le_bytes()).ok()?;
 
         assert_eq!(
             buf.len(),
             32,
             "32|2|BlockAlign|== NumChannels * BitsPerSample/8"
         );
-        buf.write(&(block_align as u16).to_le_bytes());
+        buf.write(&(block_align as u16).to_le_bytes()).ok()?;
 
         assert_eq!(
             buf.len(),
             34,
             "34|2|BitsPerSample|8 bits = 8, 16 bits = 16, etc."
         );
-        buf.write(&(bits_per_sample as u16).to_le_bytes());
+        buf.write(&(bits_per_sample as u16).to_le_bytes()).ok()?;
 
         assert_eq!(
             buf.len(),
             36,
             "36|4|Subchunk2ID|Contains the letters 'data'"
         );
-        buf.write(b"data");
+        buf.write(b"data").ok()?;
 
         assert_eq!(
             buf.len(),
             40,
             "40|4|Subchunk2Size|== NumSamples * NumChannels * BitsPerSample/8"
         );
-        buf.write(&(buflen as u32).to_le_bytes());
+        buf.write(&(buflen as u32).to_le_bytes()).ok()?;
 
         assert_eq!(buf.len(), 44, "total header length");
         Some(buf)
